2fefb95cf6f3979f56730ec8ccbaa6ab6226fb82
unknown <marpita610@gmail.com>
2021-12-31 10:50:59 +0545

    responsive completed upto mobile view

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function(){
+
+ $('.slider').slick({
+   slidesToShow: 3,
+   slidesToScroll: 1,
+   dots: true,
+   responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+ });
+
+
+});
